523d5d1c94bbf5f35f8514a9d44b29658efe2ef5
Megan Ring <meganbethring@gmail.com>
2016-02-18 22:32:20 -0800

    Initialize mongoDB with confirmation of open

--- server/server.js
+++ server/server.js
@@ -5,6 +5,19 @@ var app = express();
 
 mongoose.connect('mongodb://localhost/fizzbuzz');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("MongoDB connection is open!");
+})
+
+app.get('/', function(req, res) {
+  res.send('Get your world!');
+});
+app.post('/', function(req, res) {
+  res.sendStatus(200);
+});
+
 app.listen(8080);
 
 module.exports = app;
